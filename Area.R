aac6c751afe42b1f81220e194d0d3eccdcb64fa7
martinoni <thiago.martinoni@usp.br>
2019-09-10 14:57:16 -0300

    grafico do fossa

--- Area.R
+++ Area.R
@@ -1,14 +1,16 @@
+library(magrittr)
 library(sp)
 library(adehabitatHR)
 library(scales)
 library(png)
 library(foreach)
+library(plotly)
 
-jogo1ime <- read.delim("C:/Users/lucas/Downloads/R descr/2019-2/Cea2/jogo2acao.txt", header=TRUE)
+jogo1ime <- read.delim("~/Development/IME/cea2/jogo2acao.txt", header=TRUE)
 jogo1ime <- na.omit(jogo1ime)
 
-setwd('C:/Users/lucas/Downloads/R descr/2019-2/Cea2/imagens')
-ima <- readPNG('background_campo.png')
+setwd('~/Development/IME/cea2/')
+ima <- readPNG('./campo/background_campo.png')
 
 area.EA <- c()
 area.EB <- c()
@@ -42,3 +44,72 @@ for(i in 1:nrow(jogo1ime)){
   area.EA <- c(area.EA,jogo.mcp$area[1])
   area.EB <- c(area.EB,jogo.mcp$area[2])
 }
+
+
+areas <- cbind(area.EA, area.EB) %>% as.data.frame()
+colnames(areas) <- c('AA', 'AB')
+
+jogo1ime <- cbind(jogo1ime, areas)
+jogo1ime <- cbind(1:nrow(jogo1ime), jogo1ime)
+colnames(jogo1ime) <- c('tempo', colnames(jogo1ime)[2:ncol(jogo1ime)])
+
+attach(jogo1ime)
+
+
+
+
+
+jogo1ime_escalado <- jogo1ime[1:100,]
+jogo1ime_escalado$ataque <- as.numeric(jogo1ime_escalado$ataque)
+attach(jogo1ime_escalado)
+
+# for(i in 1:nrow(jogo1ime_escalado)){
+#   jogo1ime_escalado$ataque[i] <- sprintf('%s_%s', 
+#                                          jogo1ime_escalado$ataque[i],
+#                                          jogo1ime_escalado$posse[i])
+# }
+ataque_posseEA <- c(1)
+ataque_posseEB <- c(1)
+for(i in 2:nrow(jogo1ime_escalado)){
+  if(posse[i] == 'EA'){
+    ataque_posseEA <- c(ataque_posseEA, ataque[i])
+    ataque_posseEB <- c(ataque_posseEB, ataque_posseEB[i-1])
+  } else if(posse[i] == 'EB'){
+    ataque_posseEA <- c(ataque_posseEA, ataque_posseEA[i-1])
+    ataque_posseEB <- c(ataque_posseEB, ataque[i])}
+}
+jogo1ime_escalado <- cbind(jogo1ime_escalado, ataque_posseEA, ataque_posseEB)
+
+
+plot(AA~tempo, xlim = c(0, nrow(jogo1ime_escalado)), 
+            ylim = c(0.02464802, 0.2522278), data = jogo1ime[1,], 
+     col = 'red', lty=0, type = 'l')
+
+for(tempo in jogo1ime_escalado$tempo[2:nrow(jogo1ime_escalado)]){
+
+  if(jogo1ime_escalado[tempo,]$posse == 'EA'){
+    linha_EA <- 1
+    linha_EB <- 2
+  } else{
+    linha_EA <- 2
+    linha_EB <- 1
+  }
+  
+  lines(AA~tempo, data=jogo1ime_escalado[c(tempo, tempo-1),], col='red',
+        lty = linha_EA)
+  lines(AB~tempo, data=jogo1ime_escalado[c(tempo, tempo-1),], col='blue',
+        lty = linha_EB)
+  
+  if(ataque_posseEA[tempo] != ataque_posseEA[tempo-1]){
+    abline(v=tempo, lty = 3, col = 'red')
+    text(x=tempo, y=0.02464803, labels = desf_ataque[tempo], col='red')
+  }
+  if(ataque_posseEB[tempo] != ataque_posseEB[tempo-1]){
+    abline(v=tempo, lty = 3, col = 'blue')
+    text(x=tempo, y=0.02464803, labels = desf_ataque[tempo], col='blue')
+  }
+}
+
+legend(0, 0.25, col = 'red', legend = 'TIME A', box.col = 'red', border = 'red', fill = 'red', cex = 0.7)
+legend(0, 0.225, col = 'blue', legend = 'TIME B', box.col = 'blue', border = 'blue', fill = 'blue', cex = 0.7)
+
