d691878631a5aa50fd232a9279d7ce80ccad2e31
martinoni <thiago.martinoni@usp.br>
2019-10-10 08:19:16 -0300

    antes do primeiro relatorio

--- descritiva/boxplots.R
+++ descritiva/boxplots.R
@@ -24,8 +24,8 @@ defesas <- cbind(defesas, desfechos)
 
 
 colnames(ataques) <- c(colnames(ataques)[1:length(colnames(ataques))-1], 'Desfecho')
-colnames(ataques) <- c('Ações/minuto,
-                       ')
+#colnames(ataques) <- c('Ações/minuto,
+#                       ')
 ataques <- ataques[, 4:13]
 ataques <- ataques[, c(1:8, 10)]
 library(BBmisc)
@@ -38,21 +38,5 @@ ggplot(data = df.m, aes(x=variable, y=value)) + geom_boxplot(aes(fill=Desfecho))
   xlab('Variável') +
   ylab('Valor padronizado') +
   ylim(c(-3, 3)) +
-  theme(axis.text.x=element_text(color = "black", size=11, angle=30, vjust=.8, hjust=0.8)) +
+  theme(axis.text.x=element_text(color = "black", size=11, angle=30, vjust=.8, hjust=0.8)) 
   
-
-
-
-variaveis <- 
-  plot(1:71, lista$centAx, data = lista,
-       col = lista$V2)
-for(i in 1:71){
-  lines(i:(i+1), lista$centAx[i:(i+1)], data=lista,
-        col = lista$V2[i])
-}
-
-
-library(gamlss)
-
-model <- gamlss(V2~., family = BI('logit'), data = lista,
-                sigma.formula = ~ac_d)
